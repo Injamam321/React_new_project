059bcab4e6cbfa169af248404da25476fc043b0d
Md. Injamam Hossain <injamam075@gmail.com>
2025-11-07 18:15:05 +0600

    activelink full done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,23 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className="bg-gradient-to-r from-red-800 via-rose-700 to-pink-600 
-text-white text-2xl text-center py-6 font-semibold tracking-wide shadow-md hover:shadow-lg transition-all duration-300 hover:scale-105">
-            © {new Date().getFullYear()} Imu’s Website — All Rights Reserved ❤️
+        //         <div className="bg-gradient-to-r from-red-800 via-rose-700 to-pink-600
+        // text-white text-2xl text-center py-6 font-semibold tracking-wide shadow-md hover:shadow-lg transition-all duration-300 hover:scale-105">
+        //             © {new Date().getFullYear()} Imu’s Website — All Rights Reserved ❤️
+        //         </div>
+
+
+        <div className="bg-gradient-to-r from-red-900 via-rose-700 to-pink-600 
+text-white text-center text-xl md:text-2xl py-6 px-4 
+font-semibold tracking-wider shadow-lg 
+hover:shadow-2xl hover:scale-[1.03] 
+transition-all duration-500 ease-in-out border-t border-rose-400/30">
+
+            <p className="mb-2">© {new Date().getFullYear()} Imu’s Website</p>
+            <p className="text-sm text-rose-200">Built with ❤️ using React & Tailwind CSS</p>
+
         </div>
+
 
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaBars } from "react-icons/fa";
 import { IoClose } from "react-icons/io5";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const [activeLink, setActiveLink] = useState('/')
+    const location = useLocation();
+
+    useEffect(() => {
+
+        setActiveLink(location.pathname || '/')
+    }, [location.pathname]);
+
+
 
     const toggleMenu = () => {
         setIsOpen(!isOpen);
+    }
+
+    const handleLinkClick = (path) => {
+        setActiveLink(path);
+
     }
 
     return (
@@ -20,7 +34,7 @@ export const Navbar = () => {
                 <div className="md:hidden">
                     <button onClick={toggleMenu}>
                         {
-                            isOpen ? <IoClose /> : <FaBars />
+                            isOpen ? <IoMdClose /> : <FaBars />
                         }
 
 
@@ -28,11 +42,62 @@ export const Navbar = () => {
                 </div>
 
                 <ul className='hidden md:flex space-x-4 md:space-x-6'>
-                    <li>    <Link to={'/'} className="hover:text-red-900"> Home </Link> </li>
-                    <li>    <Link to={'products'} className="hover:text-red-900"> Products </Link>  </li>
-                    <li>    <Link to={'blogs'} className="hover:text-red-900"> Blogs </Link>  </li>
-                    <li>    <Link to={'contact'} className="hover:text-red-900"> Contacts </Link>   </li>
-                    <li>    <Link to={'about'} className="hover:text-red-900"> About </Link></li>
+
+                    <li>
+                        <Link
+                            to="/"
+                            onClick={() => handleLinkClick('/')}
+                            className={activeLink === '/' ? 'text-green-300' : 'hover:text-red-900'}
+                        >
+                            Home
+                        </Link>
+
+                    </li>
+
+                    <li>
+                        <Link to={'/products'}
+                            onClick={() => handleLinkClick('/products')}
+                            className={activeLink === '/products' ? 'text-green-300' : 'hover:text-red-900'}
+                        >
+                            products
+                        </Link>
+
+                    </li>
+
+                    <li>
+                        <Link to={'/blogs'}
+                            onClick={() => handleLinkClick('/blogs')}
+                            className={activeLink === '/blogs' ? 'text-green-300' : 'hover:text-red-900'}
+
+                        >
+
+                            Blogs
+
+                        </Link>
+
+                    </li>
+
+
+
+                    <li>
+                        <Link to={'/contact'}
+                            onClick={() => handleLinkClick('/contact')}
+                            className={activeLink === '/contact' ? 'text-green-300' : "hover:text-red-900"}
+                        >
+                            Contacts
+
+                        </Link>
+
+                    </li>
+
+                    <li>
+                        <Link to={'/about'}
+                            onClick={() => handleLinkClick('/about')}
+                            className={activeLink === '/about' ? 'text-green-300' : "hover:text-red-900"}
+                        >
+                            about
+                        </Link>
+                    </li>
 
                 </ul>
 
@@ -62,7 +127,7 @@ export const Navbar = () => {
                 </div>
 
             </div>
-        </nav>
+        </nav >
 
     )
 }
