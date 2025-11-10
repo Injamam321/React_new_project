855e6fd5aaa57bce08dc9d0d033ea9141949ba6b
Md. Injamam Hossain <injamam075@gmail.com>
2025-11-10 17:14:59 +0600

    login & register page done

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FaGoogle, FaGithub } from "react-icons/fa";
 
 const Login = () => {
     return (
         <div className='flex items-center justify-center h-[80vh] px-4 bg-green-950 '>
 
-            <div className=' bg-white max-w-sm p-8 rounded-2xl'>
+            <div className=' bg-white max-w-sm p-8 rounded-2xl text-white'>
                 <h2 className='text-green-800 font-bold text-center text-2xl p-8 mb-6'> Login </h2>
 
                 <form className='space-y-4'>
@@ -52,12 +53,12 @@ const Login = () => {
 
 
                 <div className='flex flex-col space-y-3 mt-3 '>
-                    <button className='bg-red-600 py-2 rounded hover:bg-red-700 cursor-pointer'>
-                        Login with Google
+                    <button className='bg-red-600 py-2 rounded hover:bg-red-700 flex items-center justify-center cursor-pointer'>
+                        Login with Google <FaGoogle className='ml-1' />
                     </button>
 
-                    <button className='bg-gray-600 py-2 rounded hover:bg-gray-900 cursor-pointer'>
-                        Login with Github
+                    <button className='bg-gray-600 py-2 rounded flex items-center justify-center hover:bg-gray-900 cursor-pointer'>
+                        Login with Github <FaGithub className='ml-1' />
                     </button>
                 </div>
 
--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FaGoogle, FaGithub } from "react-icons/fa";
+
 
 const Register = () => {
     return (
@@ -82,12 +84,12 @@ const Register = () => {
 
 
                     <div className='flex flex-col space-y-3 mt-3 '>
-                        <button className='bg-red-600 py-2 rounded hover:bg-red-700 cursor-pointer'>
-                            Register with Google
+                        <button className='bg-red-600 py-2 rounded flex items-center justify-center text-white hover:bg-red-700 cursor-pointer '>
+                            Register with Google <FaGoogle className='ml-1' />
                         </button>
 
-                        <button className='bg-gray-600 py-2 rounded hover:bg-gray-900 cursor-pointer'>
-                            Register with Github
+                        <button className='bg-gray-600 py-2 rounded flex items-center justify-center text-white hover:bg-gray-900 cursor-pointer'>
+                            Register with Github <FaGithub className='ml-1' />
                         </button>
                     </div>
 
